98d687be9f1e9812b7aee0ccf933250d0a1f014b
Mert Candar <mert@tarentum.io>
2020-01-15 09:26:09 +0300

    changes for asset features

--- examples/custom_single.py
+++ examples/custom_single.py
@@ -8,7 +8,7 @@ from strategy_tester.asset import AAPL
 from strategy_tester.strategy import Strategy
 from strategy_tester.risk_management import ConstantRate
 from strategy_tester.simulate import BackTest
-from strategy_tester.utils import generate_data
+from strategy_tester.utils import generate_data, ROI, sharpe_ratio
 import numpy as np
 
 def moving_average(a, n=3) :
@@ -51,10 +51,10 @@ class MACross(Strategy):
     def decide_short_close(self,order,spot_price,timestamp,Account,exog):
         return exog[0] > exog[1]
 
-
+np.random.seed(123)
 aapl = AAPL(generate_data(100000))
 account = Account(initial_balance=1000)
-risk_man = ConstantRate(0.05,on='free_margin')
+risk_man = ConstantRate(0.01,on='free_margin')
 
 strategy = MACross(RiskManagement=risk_man,id=45450,name='ma_cross_trader')
 strategy = aapl.register(strategy)
@@ -65,5 +65,12 @@ sma = np.zeros((aapl.data.shape[0],2))
 sma[(slow_period-1):,0] = moving_average(aapl.data[:,1],slow_period)
 sma[(fast_period-1):,1] = moving_average(aapl.data[:,1],fast_period)
 
-sim = BackTest(Account=account,Strategy=strategy).run(aapl,exog=sma)
+sim = BackTest(Account=account,Strategy=strategy,track=(ROI,sharpe_ratio)).run(aapl,exog=sma)
+print(sim.tracked_results)
 print(sim.Account.balances)
+print(sim.Account.equities)
+print(sim.Account.free_margins)
+print(sim.Account.navs)
+print(sim.Account.n_inactive_orders)
+print(sim.Account.max_n_active_orders)
+print(sim.Account.time)
